75de7918c8785907874362c9b024e498fdc1d1e8
sugwan <skhrnt2945@gmail.com>
2025-10-05 21:50:19 +0900

    feat: WebMvcConfigurer 의 cors 매핑 설정 추가

---
+++ chat-api/src/main/kotlin/config/WebConfig.kt
@@ -0,0 +1,17 @@
+package config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+            .allowedHeaders("*")
+            .maxAge(3600) //1시간
+    }
+}
